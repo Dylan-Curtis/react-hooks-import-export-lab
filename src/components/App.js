16cd9ae6f1b392d870ef81a657ecfd127b3aa289
Dylan Curtis <Dcurtisj@gmail.com>
2022-12-13 18:26:30 -0800

    lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
+// import {useState} from "react"
 
 function App() {
+  
+// const [state, setState] = useState(0)
+// console.log(useState(0))
+
   return (
     <div>
       <NavBar />
